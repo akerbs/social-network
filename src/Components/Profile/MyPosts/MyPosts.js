622693e90ac978638e0d93e21ed1177a3bd11f62
Anatol Kerbs <anker2702@gmail.com>
2020-05-29 18:08:51 +0200

    action creator, action type

--- src/Components/Profile/MyPosts/MyPosts.js
+++ src/Components/Profile/MyPosts/MyPosts.js
@@ -1,6 +1,10 @@
 import React from "react";
 import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
+import {
+  addPostActionCreator,
+  updateNewPostTextActionCreator,
+} from "../../../redux/state";
 
 const MyPosts = (props) => {
   let postsElements = props.posts.map((p) => (
@@ -11,13 +15,15 @@ const MyPosts = (props) => {
 
   let addPost = () => {
     // props.addPost();
-    props.dispatch({ type: "ADD-POST" });
+    // props.dispatch({ type: "ADD-POST" };
+    props.dispatch(addPostActionCreator());
   };
 
   let onPostChange = () => {
     let text = newPostElement.current.value;
     // props.updateNewPostChange(text);
-    props.dispatch({ type: "UPDATE-NEW-POST-CHANGE", newText: text });
+    // props.dispatch({ type: "UPDATE-NEW-POST-TEXT", newText: text });
+    props.dispatch(updateNewPostTextActionCreator(text));
   };
 
   return (
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,6 @@
+const ADD_POST = "ADD-POST";
+const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
+
 let store = {
   _state: {
     profilePage: {
@@ -51,7 +54,7 @@ let store = {
   //   this._callSubscruber(this._state);
   // },
   dispatch(action) {
-    if (action.type === "ADD-POST") {
+    if (action.type === ADD_POST) {
       let newPost = {
         id: 5,
         message: this._state.profilePage.newPostText,
@@ -60,12 +63,20 @@ let store = {
       this._state.profilePage.posts.push(newPost);
       this._state.profilePage.newPostText = "";
       this._callSubscruber(this._state);
-    } else if (action.type === "UPDATE-NEW-POST-CHANGE") {
+    } else if (action.type === UPDATE_NEW_POST_TEXT) {
       this._state.profilePage.newPostText = action.newText;
       this._callSubscruber(this._state);
     }
   },
 };
+
+// utility (software) designed to help to create a correct action object
+export const addPostActionCreator = () => ({ type: ADD_POST });
+
+export const updateNewPostTextActionCreator = (text) => ({
+  type: UPDATE_NEW_POST_TEXT,
+  newText: text,
+});
 
 export default store;
 
