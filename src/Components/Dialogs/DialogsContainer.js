44daa354179bf0f43c087ab4b3cba1006c2f3fd7
Anatol Kerbs <anker2702@gmail.com>
2020-06-01 09:58:16 +0200

    stateCopy in profile-reducer and dialog-reducer

--- src/Components/Dialogs/DialogsContainer.js
+++ src/Components/Dialogs/DialogsContainer.js
@@ -1,34 +1,9 @@
-import React from "react";
 import {
   sendMessageCreator,
   updateNewMessageBodyCreator,
 } from "../../redux/dialogs-reducer";
 import Dialogs from "./Dialogs";
-// import StoreContext from "../../StoreContext";
 import { connect } from "react-redux";
-
-// const DialogsContainer = () => {
-//   return (
-//     <StoreContext.Consumer>
-//       {(store) => {
-//         let onSendMessageClick = () => {
-//           store.dispatch(sendMessageCreator());
-//         };
-
-//         let onNewMessageChange = (body) => {
-//           store.dispatch(updateNewMessageBodyCreator(body));
-//         };
-//         return (
-//           <Dialogs
-//             updateNewMessageBody={onNewMessageChange}
-//             sendMessage={onSendMessageClick}
-//             dialogsPage={store.getState().dialogsPage}
-//           />
-//         );
-//       }}
-//     </StoreContext.Consumer>
-//   );
-// };
 
 let mapStateToProps = (state) => {
   return {
