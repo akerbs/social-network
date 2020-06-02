9e2097a7852e193ad30ac4be1b7f102f9ae4e99d
Anatol Kerbs <anker2702@gmail.com>
2020-06-02 14:55:25 +0200

    Users API

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -1,46 +1,32 @@
 import React from "react";
 import s from "./Users.module.css";
+import * as axios from "axios";
+import userPhoto from "../../assets/images/user.png";
 
 let Users = (props) => {
-  if (props.users.length === 0) {
-    props.setUsers([
-      {
-        id: 1,
-        photoUrl:
-          "https://images-na.ssl-images-amazon.com/images/I/61o8HELSE2L._AC_SL1080_.jpg",
-        followed: false,
-        fullName: "Anatol K.",
-        status: "I am a web developer",
-        location: { city: "Hamburg", country: "Germany" },
-      },
-      {
-        id: 2,
-        photoUrl:
-          "https://images-na.ssl-images-amazon.com/images/I/61o8HELSE2L._AC_SL1080_.jpg",
-        followed: true,
-        fullName: "Galina K.",
-        status: "I'm at home :)",
-        location: { city: "Yeysk", country: "Russia" },
-      },
-      {
-        id: 3,
-        photoUrl:
-          "https://images-na.ssl-images-amazon.com/images/I/61o8HELSE2L._AC_SL1080_.jpg",
-        followed: true,
-        fullName: "Larisa K.",
-        status: "Summer!!!",
-        location: { city: "Genue", country: "Italy" },
-      },
-    ]);
-  }
+  const getUsers = () => {
+    if (props.users.length === 0) {
+      axios
+        .get("https://social-network.samuraijs.com/api/1.0/users")
+        .then((response) => {
+          debugger;
+          props.setUsers(response.data.items);
+        });
+    }
+  };
 
   return (
     <div>
+      <button onClick={getUsers}>Get Users</button>
       {props.users.map((u) => (
         <div key={u.id}>
           <span>
             <div>
-              <img src={u.photoUrl} className={s.userPhoto} alt="userPhoto" />
+              <img
+                src={u.photos.small !== null ? u.photos.small : userPhoto}
+                className={s.userPhoto}
+                alt="userPhoto"
+              />
             </div>
             <div>
               {u.followed ? (
@@ -64,12 +50,12 @@ let Users = (props) => {
           </span>
           <span>
             <span>
-              <div>{u.fullName}</div>
+              <div>{u.name}</div>
               <div>{u.status}</div>
             </span>
             <span>
-              <div>{u.location.country}</div>
-              <div>{u.location.city}</div>
+              <div>{"u.location.country"}</div>
+              <div>{"u.location.city"}</div>
             </span>
           </span>
         </div>
