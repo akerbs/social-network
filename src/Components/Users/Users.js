ccbe97ad2b0c778972a11aed444ad1ba87084bba
Anatol Kerbs <anker2702@gmail.com>
2020-06-03 11:36:01 +0200

    Users pagination

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -1,5 +1,5 @@
 import React from "react";
-import s from "./Users.module.css";
+import styles from "./Users.module.css";
 import * as axios from "axios";
 import userPhoto from "../../assets/images/user.png";
 
@@ -10,23 +10,59 @@ class Users extends React.Component {
 
   componentDidMount() {
     axios
-      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&${this.props.pageSize}`
+      )
       .then((response) => {
-        debugger;
         this.props.setUsers(response.data.items);
+        this.props.setTotalUsersCount(response.data.totalCount);
       });
   }
 
+  onPageChanged = (pageNumber) => {
+    this.props.setCurrentPage(pageNumber);
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&${this.props.pageSize}`
+      )
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+      });
+  };
+
   render() {
+    let pagesCount = Math.ceil(
+      this.props.totalUsersCount / this.props.pageSize
+    );
+
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+      pages.push(i);
+    }
+
     return (
       <div>
+        <div>
+          {pages.map((p) => {
+            return (
+              <span
+                className={this.props.currentPage === p && styles.selectedPage}
+                onClick={(e) => {
+                  this.onPageChanged(p);
+                }}
+              >
+                {p}
+              </span>
+            );
+          })}
+        </div>
         {this.props.users.map((u) => (
           <div key={u.id}>
             <span>
               <div>
                 <img
                   src={u.photos.small !== null ? u.photos.small : userPhoto}
-                  className={s.userPhoto}
+                  className={styles.userPhoto}
                   alt="userPhoto"
                 />
               </div>
--- src/Components/Users/UsersContainer.js
+++ src/Components/Users/UsersContainer.js
@@ -1,10 +1,19 @@
 import Users from "./Users";
 import { connect } from "react-redux";
-import { followAC, unfollowAC, setUsersAC } from "../../redux/users-reducer";
+import {
+  followAC,
+  unfollowAC,
+  setUsersAC,
+  setCurrentPageAC,
+  setTotalUsersCountAC,
+} from "../../redux/users-reducer";
 
 let mapStateToProps = (state) => {
   return {
     users: state.usersPage.users,
+    pageSize: state.usersPage.pageSize,
+    totalUsersCount: state.usersPage.totalUsersCount,
+    currentPage: state.usersPage.currentPage,
   };
 };
 
@@ -13,12 +22,17 @@ let mapDispatchToProps = (dispatch) => {
     follow: (userId) => {
       dispatch(followAC(userId));
     },
-
     unfollow: (userId) => {
       dispatch(unfollowAC(userId));
     },
     setUsers: (users) => {
       dispatch(setUsersAC(users));
+    },
+    setCurrentPage: (pageNumber) => {
+      dispatch(setCurrentPageAC(pageNumber));
+    },
+    setTotalUsersCount: (totalCount) => {
+      dispatch(setTotalUsersCountAC(totalCount));
     },
   };
 };
