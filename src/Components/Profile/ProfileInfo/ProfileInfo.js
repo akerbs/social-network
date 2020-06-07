091ed4c90d441a8448b396cec1472610a901dc1d
Anatol Kerbs <anker2702@gmail.com>
2020-06-07 14:07:23 +0200

    Profile status, status, editMode, local state

--- src/Components/Profile/ProfileInfo/ProfileInfo.js
+++ src/Components/Profile/ProfileInfo/ProfileInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import s from "./ProfileInfo.module.css";
 import Preloader from "../../common/Preloader/Preloader";
+import ProfileStatus from "./ProfileStatus";
 
 const ProfileInfo = (props) => {
   if (!props.profile) {
@@ -9,14 +10,16 @@ const ProfileInfo = (props) => {
 
   return (
     <div>
-      <div>
+      {/* <div>
         <img
           src="https://cdn.civitatis.com/alemania/hamburgo/free-tour-hamburgo.jpg"
           alt="img"
         />
+      </div> */}
+      <div className={s.descriptionBlock}>
+        <img src={props.profile.photos.large} />
+        <ProfileStatus status={"Hello my friends"} />
       </div>
-      <div className={s.descriptionBlock}>ava + decs</div>
-      <img src={props.profile.photos.large} />
     </div>
   );
 };
